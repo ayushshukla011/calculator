b197bb6ffac61ceff174d844d96feee4453246d4
Ayush <ayushshukla3999@gmail.com>
2022-11-01 12:04:42 +0530

    submitting a fully completed calculator

---
+++ script.js
@@ -0,0 +1,32 @@
+let string="";
+
+let result=document.getElementById("screen");
+buttons=document.querySelectorAll(".button")
+
+Array.from(buttons).forEach((button)=>{button.addEventListener('click',(e)=> {
+    buttontext=e.target.innerText;
+    console.log('text on this button'+""+buttontext);
+
+    if (buttontext== 'X') {buttontext= '*';
+    string += buttontext;
+    result.value=string ;  
+    }
+
+    else if (buttontext=='=') {string=eval(string);
+        result.value=eval(string)  
+    }
+    
+    else if (buttontext=='C') {string='';
+    result.value=string;
+     }
+    
+     else {string+=buttontext;
+    result.value=string }
+}
+
+)})
+
+function d() {string=string.slice(0,-1);
+     result.value=string
+    
+}
